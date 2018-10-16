72323e0de37d68f5c17b039e9cac270f3e3aacb4
Valentin Raille <Valrail78@gmail.com>
2018-10-16 16:48:56 +0200

    menu

---
+++ lab.js
@@ -0,0 +1,7 @@
+console.log("hello")
+const menu = document.getElementById('menu');
+const toggle = document.querySelector('.toggle');
+menu.addEventListener("click", (event) => {
+  toggle.classList.toggle("showm");
+  toggle.classList.toggle("");
+});
