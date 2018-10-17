4164b36e1b91712b3fef9b0659160c4e4b5f49d2
Valentin Raille <Valrail78@gmail.com>
2018-10-17 10:45:07 +0200

    Add the function to switch for both menu in js

--- lab.js
+++ lab.js
@@ -1,7 +1,12 @@
 console.log("hello")
-const menu = document.getElementById('menu');
-const toggle = document.querySelector('.toggle');
-menu.addEventListener("click", (event) => {
-  toggle.classList.toggle("showm");
-  toggle.classList.toggle("");
-});
+const menu1 = document.getElementById('menu1');
+const menu2 = document.getElementById('menu2');
+const toggle1 = document.getElementById('toggle1');
+const toggle2 = document.getElementById('toggle2');
+
+menu1.addEventListener("click", function() { yo(toggle1) });
+menu2.addEventListener("click", function() { yo(toggle2) });
+
+function yo(which) {
+  which.classList.toggle("showm");
+}
