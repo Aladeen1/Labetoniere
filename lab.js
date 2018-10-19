ec31a5297419f2f24dbf3af26c2650573f00105e
Valentin Raille <Valrail78@gmail.com>
2018-10-19 16:40:29 +0200

    ménage

--- lab.js
+++ lab.js
@@ -1,12 +1,7 @@
 console.log("hello")
+
 const dropTarget = document.getElementById('dropTarget');
-const shareTarget = document.getElementById('shareTarget');
 const dropApparition = document.getElementById('dropApparition');
-const shareApparition = document.getElementById('shareApparition');
-
-
-dropTarget.addEventListener("click", function() { slideDrop(dropApparition) });
-shareTarget.addEventListener("click", function() { slideShare(shareApparition) });
 
 function slideDrop(menuActif) {
   menuActif.classList.toggle("notHiddenPosition");
@@ -18,6 +13,11 @@ function slideDrop(menuActif) {
   }
 }
 
-function slideShare(menuActif) {
-  menuActif.classList.toggle("positionPasCachée");
-}
+dropTarget.addEventListener("click", function() { slideDrop(dropApparition) });
+
+// function slideShare(menuActif) {
+//   menuActif.classList.toggle("positionPasCachée");
+// }
+// shareTarget.addEventListener("click", function() { slideShare(shareApparition) });
+// const shareApparition = document.getElementById('shareApparition');
+//const shareTarget = document.getElementById('shareTarget');
